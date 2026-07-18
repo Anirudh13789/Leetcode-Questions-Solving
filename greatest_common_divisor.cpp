3e676e06c36658ebee5ecb79998585bbf5aa0515
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-07-18 19:08:48 +0530

    solved daily question

---
+++ greatest_common_divisor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int gcd(int a,int b){
+        return b==0?a:gcd(b,a%b);
+    }
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return gcd(nums[0],nums[nums.size()-1]);
+    }
+};
