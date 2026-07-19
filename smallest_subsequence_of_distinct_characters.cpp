f44a8a3bb362574ac9af56caf650ba9b0910de94
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-07-19 21:29:48 +0530

    completed daily ques

---
+++ smallest_subsequence_of_distinct_characters.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int freq[26]={0};
+        bool visited[26]={false};
+
+        for(char c:s){
+            freq[c-'a']++;
+        }
+        string st;
+
+        for (char c: s){
+            freq[c-'a']--;
+
+            if(visited[c-'a']){
+                continue;
+            }
+
+            while(!st.empty() && st.back()>c && freq[st.back()-'a']>0){
+                visited[st.back()-'a']=false;
+                st.pop_back();
+            }
+            st.push_back(c);
+            visited[c-'a']=true;
+        }
+        return st;
+    }
+};
