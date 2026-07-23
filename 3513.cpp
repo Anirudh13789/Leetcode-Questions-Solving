35b506e42d27e3a37bfdbcb296b31f8444b0a407
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-07-23 19:01:05 +0530

    daily prb solved

---
+++ 3513.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <bit>
+using namespace std;
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        size_t n = nums.size();
+        return 1 << (bit_width(n) - 3 / (n + 1));
+    }
+};
