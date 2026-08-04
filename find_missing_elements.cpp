436446eb9087bb400aba1fe8dabbb4f89e18fa77
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-08-04 10:35:10 +0530

    daily prb solved

---
+++ find_missing_elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> ans;
+        for(int i=0;i+1<nums.size();i++){
+            if(nums[i]+1 != nums[i+1]){
+                for(int j=nums[i]+1;j<nums[i+1];j++){
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
