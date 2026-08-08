1dcd1766110850eac4d5c3e1a98f5999df4bd3ea
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-08-08 10:33:23 +0530

    completed daily goal

---
+++ string_to_int.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+using namespace std;
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        int n=s.length();
+        while(i<n && s[i]==' '){
+            i++;
+        }
+        int sign=1;
+        if(i<n && (s[i]=='+' || s[i]=='-')){
+            if(s[i]=='-'){
+                sign=-1;
+            }
+            i++;
+        }
+        long long ans=0;
+        while(i<n && s[i]>='0' && s[i]<='9'){
+            ans=ans*10+(s[i]-'0');
+            if(sign==1 && ans>INT_MAX){
+                return INT_MAX;
+            }
+            if(sign==-1 && -ans<INT_MIN){
+                return INT_MIN;
+            }
+            i++;
+        }
+        return (int)(sign*ans);
+    }
+};
