e842827fda390f3719a5387155da888c56fc5592
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-08-12 09:55:06 +0530

    daily ques solved

---
+++ 2958.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int, int> freq; 
+        int left=0;
+        int maxlen=0;
+        for(int right=0;right<nums.size();right++){
+            freq[nums[right]]++;
+            while (freq[nums[right]]>k){
+                freq[nums[left]]--;
+                left++;
+            }
+            maxlen=max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+};
