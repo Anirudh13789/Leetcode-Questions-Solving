426dd395265e523ad4213c121746da1de2022cbc
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-03 07:29:25 +0530

    solved daily prb

---
+++ construct_uniform_parity_array_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int smallestOdd=INT_MAX;
+        for(int num:nums1){
+            if(num%2!=0){
+                smallestOdd=min(smallestOdd,num);
+            }
+        }
+        if(smallestOdd==INT_MAX){
+            return true;
+        }
+        for(int num:nums1){
+            if(num%2==0 && num<=smallestOdd){
+                return false;
+            }
+        }
+        return true;
+    }
+};
