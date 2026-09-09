447cafe0c44547a3b1ab86858a60c612a9c56a74
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-09 10:38:02 +0530

    daily prb solved

---
+++ count_commas_in_range_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0;
+        for(long long x=1000;x<=n;x*=1000){
+            ans+=n-x+1;
+        }
+        return ans;
+    }
+};
