00818d9cef8d34ae3b270961127c863ab65c5ab4
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-10 17:44:55 +0530

    solved daily prb

---
+++ count_nodes_equal_to_avg.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class TreeNode{
+public:
+    TreeNode*left;
+    TreeNode*right;
+    int val;
+    TreeNode(int v){
+        val=v;
+        left=right=NULL;
+    }
+};
+class Solution {
+public:
+    int ans=0;
+    pair<int,int> dfs(TreeNode* root){
+        if(root==NULL){
+            return {0,0};
+        }
+        pair<int,int> left=dfs(root->left);
+        pair<int,int> right=dfs(root->right);
+        int sum=left.first+right.first+root->val;
+        int count=left.second+right.second+1;
+        if(sum/count==root->val){
+            ans++;
+        }
+        return {sum,count};
+    }
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
