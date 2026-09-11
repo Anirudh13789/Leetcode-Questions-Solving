4d25c7180db8a284d6be26ffa103d72a1ee2fc81
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-11 17:52:13 +0530

    solved daily prb

---
+++ unique_3_digit_even_number.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int cnt[10]={};
+        for(int x:digits){
+            cnt[x]++;
+        }
+        int ans=0;
+        for(int num=100;num<=999;num++){
+            if(num%2!=0){
+                continue;
+            }
+            int x=num;
+            int a=x/100;
+            int b=(x/10)%10;
+            int c=x%10;
+            int need[10]={};
+            need[a]++;
+            need[b]++;
+            need[c]++;
+            bool possible=true;
+            for(int i=0;i<10;i++){
+                if(need[i]>cnt[i]){
+                    possible=false;
+                    break;
+                }
+            }
+            if(possible){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
