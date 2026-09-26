3c155a15e62dfba0fe87b41b7733d27021cf0813
Anirudh13789 <96854440+Anirudh13789@users.noreply.github.com>
2026-09-26 12:17:09 +0530

    solved daily prb

---
+++ 1807.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution{
+public:
+    string evaluate(string s,auto& k){
+        unordered_map<string, string> d;
+        for(auto& k: k){
+            d[k[0]]=k[1];
+        }
+        string res;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                int j=s.find(')',i+1);
+                auto t=s.substr(i+1,j-i-1);
+                res+=d.count(t) ? d[t] : "?";
+                i=j;
+            }
+            else{
+                res+=s[i];
+            }
+        }
+        return res;
+    }
+};
